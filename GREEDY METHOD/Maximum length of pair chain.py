3ff8e72dec375154860832cf0a8ea50c2855b54d
anirudhvutukuru <153755629+anirudhvutukuru@users.noreply.github.com>
2024-04-24 11:52:52 +0530

    Add files via upload

---
+++ GREEDY METHOD/Maximum length of pair chain.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findLongestChain(self, pairs: List[List[int]]) -> int:
+        pair.sort(key=lambda x:x[1])
+        count=1
+        prev_end=pairs[0][1]
+        i=1
+        while i<len(pairs):
+            if pairs[i][0]>prev_end:
+                count+=1
+                prev_end=pairs[i]
+        
